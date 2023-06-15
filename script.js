b0067b4351c40e734427b07a96d1942a11d74984
Daniel Aguilar <danjagmeta@meta.com>
2023-06-15 14:23:20 -0700

    Current movie Posters successfully displaying on page

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("hello!")
+
+var submitButton =  document.getElementById("submitButton");
+var userSearch = document.getElementById("userSearch");
+var movieResults = document.getElementById("movieResults");
+
+document.addEventListener("DOMContentLoaded", async (event) =>{
+    event.preventDefault();
+    console.log("It worked! :D");
+    
+    try{
+        const api_key = "7284545292fe355c5ef22c4cfa1616a6"
+        const pulledMovies = movieResults.value;
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=1}`
+        /*const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Mjg0NTQ1MjkyZmUzNTVjNWVmMjJjNGNmYTE2MTZhNiIsInN1YiI6IjY0OGE0NzcxYTgwMjM2MDBhZDk2MzQxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AK7LPaanYH7dwAzvYVEwSXa2tNBNw7I2PoXFGFY1TLs'
+            }
+          };
+          */
+        const response = await fetch(url);
+        const data = await response.json();
+        const results = data.results;
+        console.log(results)
+        
+        movieResults.innerHTML="";
+        results.forEach(movie => {
+            const poster = document.createElement("img");
+            poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
+            poster.alt = movie.title;
+            movieResults.appendChild(poster)
+        });
+
+        console.log(data);
+
+
+            
+    }
+    catch{
+        console.log("Something isn't right...")
+    }
+})
