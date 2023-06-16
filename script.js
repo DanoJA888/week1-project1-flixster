62e4d5789c76f9e4073ac0932d2dc4d4ddc79f0c
Daniel Aguilar <danjagmeta@meta.com>
2023-06-15 22:27:17 -0700

    Load more pages button feature implemented and running

--- script.js
+++ script.js
@@ -2,57 +2,95 @@ console.log("hello!")
 
 var submitButton =  document.getElementById("submitButton");
 var userSearch = document.getElementById("userSearch");
-var movieResults = document.getElementById("movieResults");
+var movieGrid = document.getElementById("movie-grid");
+
+var pages = 1;
+var loadMoreMoviesBtn = document.getElementById("load-more-movies-btn")
+
+
+
 
 document.addEventListener("DOMContentLoaded", async (event) =>{
     event.preventDefault();
     console.log("It worked! :D");
+    movieGrid.innerHTML="";
     
     try{
         const api_key = "7284545292fe355c5ef22c4cfa1616a6"
-        const pulledMovies = movieResults.value;
-        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=1}`
-        /*const options = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Mjg0NTQ1MjkyZmUzNTVjNWVmMjJjNGNmYTE2MTZhNiIsInN1YiI6IjY0OGE0NzcxYTgwMjM2MDBhZDk2MzQxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AK7LPaanYH7dwAzvYVEwSXa2tNBNw7I2PoXFGFY1TLs'
-            }
-          };
-          */
+        const pulledMovies = movieGrid.value;
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`
+
         const response = await fetch(url);
         const data = await response.json();
         const results = data.results;
         console.log(results)
         
-        movieResults.innerHTML="";
         results.forEach(movie => {
+            const movieCard = document.createElement("div");
 
             const title = document.createElement("p");
             title.textContent = movie.title;
-            movieResults.appendChild(title)
+            movieCard.appendChild(title)
 
-
-            const containPoster = document.createElement("div");
             const poster = document.createElement("img");
-            poster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
             poster.alt = movie.title;
-            containPoster.appendChild(poster)
-            movieResults.appendChild(containPoster)
-
-            
-
+            movieCard.appendChild(poster)
+        
             const rating = document.createElement("p")
             rating.textContent = "⭐️" + movie.vote_average +"⭐️";
-            movieResults.appendChild(rating)
+            movieCard.appendChild(rating)
+
+            movieCard.classList.add("poster-format")
+            movieGrid.appendChild(movieCard)
         });
 
         console.log(data);
-
-
-            
     }
     catch{
         console.log("Something isn't right...")
     }
+})
+
+loadMoreMoviesBtn.addEventListener("click", async (event) =>{
+    event.preventDefault();
+    console.log("refresh prevented for load more movies")
+    pages++;
+    console.log(pages);
+    try{
+        const api_key = "7284545292fe355c5ef22c4cfa1616a6"
+        const pulledMovies = movieGrid.value;
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`
+
+        const response = await fetch(url);
+        const data = await response.json();
+        const results = data.results;
+        console.log(results)
+        
+        results.forEach(movie => {
+            const movieCard = document.createElement("div");
+
+            const title = document.createElement("p");
+            title.textContent = movie.title;
+            movieCard.appendChild(title)
+
+            const poster = document.createElement("img");
+            poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
+            poster.alt = movie.title;
+            movieCard.appendChild(poster)
+        
+            const rating = document.createElement("p")
+            rating.textContent = "⭐️" + movie.vote_average +"⭐️";
+            movieCard.appendChild(rating)
+
+            movieCard.classList.add("poster-format")
+            movieGrid.appendChild(movieCard)
+        });
+
+        console.log(data);
+    }
+    catch{
+        console.log("something is wrong with the load more movies button")
+    }
+
 })
