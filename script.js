ac2c9887d163b4f6b0ae989068e660b6d0f210c7
Daniel Aguilar <danjagmeta@meta.com>
2023-06-15 22:58:23 -0700

    Changed loadMovies async function to take in url as a parameter for further use of function when searching for movies using different api urls

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 console.log("hello!")
 
-var submitButton =  document.getElementById("submitButton");
-var userSearch = document.getElementById("userSearch");
+var api_key = "7284545292fe355c5ef22c4cfa1616a6";
+
+var searchnButton =  document.getElementById("search-button");
+var userSearch = document.getElementById("search-input");
 var movieGrid = document.getElementById("movie-grid");
 
 var pages = 1;
 var loadMoreMoviesBtn = document.getElementById("load-more-movies-btn")
 
 
-async function loadMovies(){
-    const api_key = "7284545292fe355c5ef22c4cfa1616a6"
-        const pulledMovies = movieGrid.value;
-        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`
+async function loadMovies(action_url){
+    const pulledMovies = movieGrid.value;
+    const url = action_url;
 
-        const response = await fetch(url);
-        const data = await response.json();
-        const results = data.results;
-        console.log(results)
-        
-        results.forEach(movie => {
-            const movieCard = document.createElement("div");
+    const response = await fetch(url);
+    const data = await response.json();
+    const results = data.results;
+    console.log(results)
+    
+    results.forEach(movie => {
+        const movieCard = document.createElement("div");
 
-            const title = document.createElement("p");
-            title.textContent = movie.title;
-            movieCard.appendChild(title)
+        const title = document.createElement("p");
+        title.textContent = movie.title;
+        movieCard.appendChild(title)
 
-            const poster = document.createElement("img");
-            poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
-            poster.alt = movie.title;
-            movieCard.appendChild(poster)
-        
-            const rating = document.createElement("p")
-            rating.textContent = "⭐️" + movie.vote_average +"⭐️";
-            movieCard.appendChild(rating)
+        const poster = document.createElement("img");
+        poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
+        poster.alt = movie.title;
+        movieCard.appendChild(poster)
+    
+        const rating = document.createElement("p")
+        rating.textContent = "⭐️" + movie.vote_average +"⭐️";
+        movieCard.appendChild(rating)
 
-            movieCard.classList.add("poster-format")
-            movieGrid.appendChild(movieCard)
-        });
+        movieCard.classList.add("poster-format")
+        movieGrid.appendChild(movieCard)
+    });
 }
 
 document.addEventListener("DOMContentLoaded", async (event) =>{
@@ -45,7 +46,7 @@ document.addEventListener("DOMContentLoaded", async (event) =>{
     movieGrid.innerHTML="";
     
     try{
-        await loadMovies();
+        await loadMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`);
 
         console.log(data);
     }
@@ -60,7 +61,7 @@ loadMoreMoviesBtn.addEventListener("click", async (event) =>{
     pages++;
     console.log(pages);
     try{
-        await loadMovies();
+        await loadMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`);
 
         console.log(data);
     }
@@ -69,3 +70,10 @@ loadMoreMoviesBtn.addEventListener("click", async (event) =>{
     }
 
 })
+/* 
+searchButton.addEventListener("click", (event) =>{
+    event.preventDefault();
+
+
+})
+*/
