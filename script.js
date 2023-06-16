120066be6b125adb7e12343e35a2d2a378c63c80
Daniel Aguilar <danjagmeta@meta.com>
2023-06-16 00:30:29 -0700

    Edited paramater for laodingMovies in order to generate correct output whena search is made. Implemented search feature.

--- script.js
+++ script.js
@@ -2,42 +2,50 @@ console.log("hello!")
 
 var api_key = "7284545292fe355c5ef22c4cfa1616a6";
 
-var searchnButton =  document.getElementById("search-button");
+var searchButton =  document.getElementById("search-button");
 var userSearch = document.getElementById("search-input");
 var movieGrid = document.getElementById("movie-grid");
 
 var pages = 1;
+var pageLoadingType = 1 // this variable is used for identifying when i need to pass the "now playing url" vs the "search" url
 var loadMoreMoviesBtn = document.getElementById("load-more-movies-btn")
 
 
-async function loadMovies(action_url){
-    const pulledMovies = movieGrid.value;
-    const url = action_url;
+async function loadMovies(loadingTypeId){
+    try{
+        const pulledMovies = movieGrid.value;
+        const searchValue = userSearch.value;
+        const url = loadingTypeId === 1 ? `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}` : `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&include_adult=false&language=en-US&page=${pages}&query=${searchValue}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
-    const results = data.results;
-    console.log(results)
-    
-    results.forEach(movie => {
-        const movieCard = document.createElement("div");
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.json();
+        const results = data.results;
+        console.log(results)
+        
+        results.forEach(movie => {
+            const movieCard = document.createElement("div");
 
-        const title = document.createElement("p");
-        title.textContent = movie.title;
-        movieCard.appendChild(title)
+            const title = document.createElement("p");
+            title.textContent = movie.title;
+            movieCard.appendChild(title)
 
-        const poster = document.createElement("img");
-        poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
-        poster.alt = movie.title;
-        movieCard.appendChild(poster)
-    
-        const rating = document.createElement("p")
-        rating.textContent = "⭐️" + movie.vote_average +"⭐️";
-        movieCard.appendChild(rating)
+            const poster = document.createElement("img");
+            poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
+            poster.alt = movie.title;
+            movieCard.appendChild(poster)
+        
+            const rating = document.createElement("p")
+            rating.textContent = "⭐️" + movie.vote_average +"⭐️";
+            movieCard.appendChild(rating)
 
-        movieCard.classList.add("poster-format")
-        movieGrid.appendChild(movieCard)
-    });
+            movieCard.classList.add("poster-format")
+            movieGrid.appendChild(movieCard)
+        });
+    }
+    catch{
+        console.log("loading isnt work")
+    }
 }
 
 document.addEventListener("DOMContentLoaded", async (event) =>{
@@ -46,9 +54,8 @@ document.addEventListener("DOMContentLoaded", async (event) =>{
     movieGrid.innerHTML="";
     
     try{
-        await loadMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`);
+        await loadMovies(pageLoadingType);
 
-        console.log(data);
     }
     catch{
         console.log("Something isn't right...")
@@ -61,19 +68,31 @@ loadMoreMoviesBtn.addEventListener("click", async (event) =>{
     pages++;
     console.log(pages);
     try{
-        await loadMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`);
-
-        console.log(data);
+        await loadMovies(pageLoadingType);
     }
     catch{
         console.log("something is wrong with the load more movies button")
     }
 
 })
-/* 
-searchButton.addEventListener("click", (event) =>{
+
+searchButton.addEventListener("click", async (event) =>{
     event.preventDefault();
+    pages = 1
+    pageLoadingType = 2
+    console.log(pages)
+    console.log(userSearch.value)
+    movieGrid.innerHTML="";
+    try{
+        
+        await loadMovies(pageLoadingType);
+
+        console.log()
+    }
+    catch{
+        
+        console.log("something is wrong with the search")
+    }
 
 
 })
-*/
