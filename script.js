a943a020d206beaec545a710ac9b229c066d224b
Daniel Aguilar <danjagmeta@meta.com>
2023-06-15 22:37:30 -0700

    API call for loading movie information seperated into function for cleaner and more concise code

--- script.js
+++ script.js
@@ -8,15 +8,8 @@ var pages = 1;
 var loadMoreMoviesBtn = document.getElementById("load-more-movies-btn")
 
 
-
-
-document.addEventListener("DOMContentLoaded", async (event) =>{
-    event.preventDefault();
-    console.log("It worked! :D");
-    movieGrid.innerHTML="";
-    
-    try{
-        const api_key = "7284545292fe355c5ef22c4cfa1616a6"
+async function loadMovies(){
+    const api_key = "7284545292fe355c5ef22c4cfa1616a6"
         const pulledMovies = movieGrid.value;
         const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`
 
@@ -44,6 +37,15 @@ document.addEventListener("DOMContentLoaded", async (event) =>{
             movieCard.classList.add("poster-format")
             movieGrid.appendChild(movieCard)
         });
+}
+
+document.addEventListener("DOMContentLoaded", async (event) =>{
+    event.preventDefault();
+    console.log("It worked! :D");
+    movieGrid.innerHTML="";
+    
+    try{
+        await loadMovies();
 
         console.log(data);
     }
@@ -58,34 +60,7 @@ loadMoreMoviesBtn.addEventListener("click", async (event) =>{
     pages++;
     console.log(pages);
     try{
-        const api_key = "7284545292fe355c5ef22c4cfa1616a6"
-        const pulledMovies = movieGrid.value;
-        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=${pages}`
-
-        const response = await fetch(url);
-        const data = await response.json();
-        const results = data.results;
-        console.log(results)
-        
-        results.forEach(movie => {
-            const movieCard = document.createElement("div");
-
-            const title = document.createElement("p");
-            title.textContent = movie.title;
-            movieCard.appendChild(title)
-
-            const poster = document.createElement("img");
-            poster.src = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
-            poster.alt = movie.title;
-            movieCard.appendChild(poster)
-        
-            const rating = document.createElement("p")
-            rating.textContent = "⭐️" + movie.vote_average +"⭐️";
-            movieCard.appendChild(rating)
-
-            movieCard.classList.add("poster-format")
-            movieGrid.appendChild(movieCard)
-        });
+        await loadMovies();
 
         console.log(data);
     }
